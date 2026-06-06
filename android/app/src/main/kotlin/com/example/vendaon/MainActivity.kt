fd03f49b48710a91556270609116e87e9b46120b
Elton Cruz <sourico.br.ec@gmail.com>
2026-06-06 13:13:16 -0300

    Corrige nome do app para TORICO

--- android/app/src/main/kotlin/com/example/vendaon/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vendaon/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.vendaon
+package com.example.torico
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vendaon/main.dart';
+import 'package:torico/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
